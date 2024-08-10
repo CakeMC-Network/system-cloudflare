917f4ba369d934c6b1d905097928c2c087ec350e
lunarydess <lucielle.hoerner@outlook.com>
2024-08-10 19:25:38 +0200

    :sparkles: added editor-configs :sparkles: added formatter-rules :sparkles: added proper git-(ignore/keep)-vars

---
+++ build.gradle.kts
@@ -0,0 +1,28 @@
+import java.nio.charset.StandardCharsets
+
+plugins {
+    id("idea")
+    id("java")
+    id("java-library")
+}
+
+group = "net.cakemc"
+version = "0.0.0-develop"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+}
+
+tasks.withType<JavaCompile> {
+    sourceCompatibility = JavaVersion.VERSION_21.toString()
+    targetCompatibility = JavaVersion.VERSION_21.toString()
+    options.encoding = StandardCharsets.UTF_8.toString()
+}
+
+tasks.withType<AbstractArchiveTask> {
+    isReproducibleFileOrder = true
+    isPreserveFileTimestamps = false
+}
