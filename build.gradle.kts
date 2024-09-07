27f39bfb68ceab266b78253e7b418a5b5569b40f
lunarydess <lucielle.hoerner@outlook.com>
2024-09-07 19:53:48 +0200

    Revert ":sparkles: commited what i've done (yet)"
    This reverts commit 06fd5ebdded3e038661b5bd9a18816de594e6e4c.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,49 +1,28 @@
-import java.net.URI
 import java.nio.charset.StandardCharsets
-import java.util.*
 
 plugins {
-  id("idea")
-
-  id("java")
-  id("java-library")
-
-  id("maven-publish")
-  id("com.gradleup.shadow") version "8.3.0"
+    id("idea")
+    id("java")
+    id("java-library")
 }
 
-group = "net.cakemc.system"
-version = "0.0.0-develop" // TODO: ... | add proper versioning
-
-val repoProperties = Properties()
-repoProperties.load(file("credentials.properties").inputStream())
-
-val repoUsername: String = (repoProperties["username"] ?: System.getenv("REPOSITORY_USERNAME")).toString()
-val repoPassword: String = (repoProperties["username"] ?: System.getenv("REPOSITORY_PASSWORD")).toString()
+group = "net.cakemc"
+version = "0.0.0-develop"
 
 repositories {
-  mavenLocal()
-  mavenCentral()
-  maven {
-    name = "cakemc"
-    url = URI.create("http://cakemc.net:8081/repository/maven-releases")
-    credentials {
-      username = repoUsername
-      password = repoPassword
-    }
-    isAllowInsecureProtocol = true
-  }
+    mavenCentral()
 }
 
-dependencies {}
+dependencies {
+}
 
 tasks.withType<JavaCompile> {
-  sourceCompatibility = JavaVersion.VERSION_21.toString()
-  targetCompatibility = JavaVersion.VERSION_21.toString()
-  options.encoding = StandardCharsets.UTF_8.toString()
+    sourceCompatibility = JavaVersion.VERSION_21.toString()
+    targetCompatibility = JavaVersion.VERSION_21.toString()
+    options.encoding = StandardCharsets.UTF_8.toString()
 }
 
 tasks.withType<AbstractArchiveTask> {
-  isReproducibleFileOrder = true
-  isPreserveFileTimestamps = false
+    isReproducibleFileOrder = true
+    isPreserveFileTimestamps = false
 }
